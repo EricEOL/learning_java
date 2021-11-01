19a9cfd1455bdf71e86958149ba1d2f63109ee05
EricEOL <lopes.eric051@gmail.com>
2021-11-01 12:25:29 -0300

    Entendendo o conceito de classe abstrata

--- src/TesteReferencias.java
+++ src/TesteReferencias.java
@@ -4,15 +4,11 @@ public class TesteReferencias {
     Gerente g1 = new Gerente();
     g1.setSalario(5000);
 
-    Funcionario f1 = new Funcionario();
-    f1.setSalario(2000);
-
     EditorVideo ev1 = new EditorVideo();
     ev1.setSalario(2500);
 
     ControleBonificacao controle = new ControleBonificacao();
     controle.registra(g1);
-    controle.registra(f1);
     controle.registra(ev1);
 
     System.out.println(controle.getSoma());
