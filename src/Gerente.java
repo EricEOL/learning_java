9532b9872bddca3bfa0e26211d8d04e0c3af954d
EricEOL <lopes.eric051@gmail.com>
2021-11-04 20:52:19 -0300

    Criando classes abstratas intermediarias

--- src/Gerente.java
+++ src/Gerente.java
@@ -1,18 +1,6 @@
-public class Gerente extends Funcionario{
-  private int senha;
+public class Gerente extends FuncionarioAutenticavel{
 
-  public void setSenha(int senha) {
-    this.senha = senha;
-  }
-
-  public boolean autentica(int senha) {
-    if(this.senha == senha) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-
+  @Override
   public double getBonificacao() {
     return super.getSalario();
   }
